3f9e94463dac848784328aabdd32581cc6ec445f
ElenaPintosAlles <92037978+ElenaPintosAlles@users.noreply.github.com>
2022-05-01 12:40:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <button className="btn btn-primary"></button>
       <h1> Hello </h1>
     </div>
   );
 }
-
-export default App;
