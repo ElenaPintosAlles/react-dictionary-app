eece4ed84cafb0da75991e5f4b7855c0002b88f5
ElenaPintosAlles <92037978+ElenaPintosAlles@users.noreply.github.com>
2022-05-01 13:29:23 +0200

    added search form and fetching search input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <button className="btn btn-primary"></button>
-      <h1> Hello </h1>
+      <div className="Container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          {" "}
+          <small>Coded by Elena Pintos Alles</small>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
