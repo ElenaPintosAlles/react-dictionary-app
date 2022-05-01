07b82a4d54cea51a981474637c6d36d8a290f0de
ElenaPintosAlles <92037978+ElenaPintosAlles@users.noreply.github.com>
2022-05-01 16:01:33 +0200

    adjusted synonyms code API

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -7,14 +8,18 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
+            <h4>
               {definition.definition}
               <br />
+            </h4>
+            <h4>
               <em>{definition.example}</em>
-            </p>
+            </h4>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
